9b8aaefd907f098383b6d76d0eac797104509d62
bautistasch <bschneeberger@itba.edu.ar>
2023-02-22 16:16:46 -0300

    Con uno que vuela poco

--- TPQUAD/source/Sistema de Control/SistemaDeControl.c
+++ TPQUAD/source/Sistema de Control/SistemaDeControl.c
@@ -8,7 +8,7 @@
 
 // Defino que el ajuste por integral no puede ser mayor a 1N de diferencia
 
-#define MAX_INTEGRAL_ERROR_NEWTON	0.4    // 0.2 Newton
+#define MAX_INTEGRAL_ERROR_NEWTON	1    // 0.2 Newton
 
 
 
